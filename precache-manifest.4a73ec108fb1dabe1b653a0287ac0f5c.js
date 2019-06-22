a40ba592f033be993d2413ac3bb4ce09f45e0120
sdavran <sumeyra.ozgur91@gmail.com>
2019-06-22 16:00:05 +0300

    Updates

---
+++ precache-manifest.4a73ec108fb1dabe1b653a0287ac0f5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7333ce7478cfebc37e6152ffeb33e8c8",
+    "url": "/random-user/index.html"
+  },
+  {
+    "revision": "4228ae2d6bcb4beae445",
+    "url": "/random-user/static/js/2.49fb7777.chunk.js"
+  },
+  {
+    "revision": "761a2b5eb4bca80d6161",
+    "url": "/random-user/static/js/main.3cb53757.chunk.js"
+  },
+  {
+    "revision": "f36225f809863f5f268c",
+    "url": "/random-user/static/js/runtime~main.38fb7cdc.js"
+  }
+]);
